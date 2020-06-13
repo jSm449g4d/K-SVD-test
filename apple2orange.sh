9126de903360b604708062bad58c09ca007ea1d1
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-06-14 06:21:59 +0900

    Add files via upload

---
+++ apple2orange.sh
@@ -0,0 +1,8 @@
+﻿
+apt install p7zip-full
+echo "download from \"Index of /~taesung_park/CycleGAN/datasets\""
+URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/apple2orange.zip
+echo $URL
+wget -N $URL
+7z x apple2orange.zip
+rm apple2orange.zip
